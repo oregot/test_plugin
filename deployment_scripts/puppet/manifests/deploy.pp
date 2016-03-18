4d5d12c3857595002be014750654cb03772764ae
Oleg Ilin <on.ilyin@gmail.com>
2016-03-18 16:26:18 +0300

    updated

--- deployment_scripts/puppet/manifests/deploy.pp
+++
@@ -1,8 +0,0 @@
-class test_plugin {
-  file { '/tmp/AAA_test_plugin':
-      owner   => 'root',
-      group   => 'root',
-      mode    => 0644,
-      content => "There are test plugin information\n",
-  }
-}
